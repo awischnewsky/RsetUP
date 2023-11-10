5dfafbca23984faca8560520ac3d4748ac0b9588
Arina Wischnewsky <71431011+awischnewsky@users.noreply.github.com>
2023-11-10 17:07:23 +0100

    Create setUP_Uni_Luxembourg.R

---
+++ setUP_Uni_Luxembourg.R
@@ -0,0 +1,14 @@
+# SetUP ------------------------------------------------------------------------
+
+#' (sets working directory, clears memory, resets, defines notation) 
+if (!("rstudioapi" %in% installed.packages())) {install.packages("rstudioapi")}
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))# set the working directory to the current R Script location 
+par(function(){dev.new(); par(no.readonly=T)}()); options(scipen=999); dev.off()
+rm(list=ls()) # comment this out to keep saved elements 
+cat("\014")
+#' Colors 
+ULblue <- rgb(40/255, 161/255, 215/255) # University of Luxembourg blue corporate color
+ULred <- rgb(219/255, 2/255, 27/255) # University of Luxembourg red corporate color
+
+#-------------------------------------------------------------------------------
+
