4298eabba352d5cb3f9658b4a02c0aa9f04294d8
Arina Wischnewsky <71431011+awischnewsky@users.noreply.github.com>
2023-11-10 17:19:12 +0100

    Update setUP.R

--- setUP.R
+++ setUP.R
@@ -2,8 +2,8 @@
 
 #' (sets working directory, clears memory, resets, defines notation) 
 if (!("rstudioapi" %in% installed.packages())) {install.packages("rstudioapi")}
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))# set the working directory to the current R Script location 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))# set the working directory to the current R script location 
 par(function(){dev.new(); par(no.readonly=T)}()); options(scipen=999); dev.off()
-rm(list=ls()) # comment this out to keep saved elements 
+rm(list=ls()) # comment this out to keep saved elements if necessary
 cat("\014")
 # ----------------------------------------------------------------------------------------------------------------------
