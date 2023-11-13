e1bba3f92b32f4e95c4ccc2afa938c95c98c1726
Arina Wischnewsky <71431011+awischnewsky@users.noreply.github.com>
2023-11-13 12:47:39 +0100

    Update setUP_Uni_Luxembourg.R

--- setUP_Uni_Luxembourg.R
+++ setUP_Uni_Luxembourg.R
@@ -6,7 +6,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))# set the working dir
 par(function(){dev.new(); par(no.readonly=T)}()); options(scipen=999); dev.off()
 rm(list=ls()) # comment this out to keep saved elements if necessary
 cat("\014")
-#' Colors 
+
+#' (colors) 
 ULblue <- rgb(40/255, 161/255, 215/255) # University of Luxembourg blue corporate color
 ULred <- rgb(219/255, 2/255, 27/255) # University of Luxembourg red corporate color
 
