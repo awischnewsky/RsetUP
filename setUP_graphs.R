faddcc80536d39904227547627c0f879ca07f633
Arina Wischnewsky <71431011+awischnewsky@users.noreply.github.com>
2023-11-15 22:00:04 +0100

    Update setUP_graphs.R

--- setUP_graphs.R
+++ setUP_graphs.R
@@ -7,13 +7,12 @@ par(function(){dev.new(); par(no.readonly=T)}()); options(scipen=999); dev.off()
 rm(list=ls()) # comment this out to keep saved elements if necessary
 cat("\014")
 
-
 #' (font and theme for plots [for Windows fonts])
 if (!("extrafont" %in% installed.packages())) {install.packages("extrafont")}
 if (.Platform$OS.type == "windows") {extrafont::loadfonts(device = "win")
   windowsFonts(C = windowsFont("Segoe UI"))} # can pick any font
-if (!("ggplot2" %in% installed.packages())) {install.packages("ggplot2")}
-ggplot2::theme_set(theme_bw() + theme(panel.border = element_blank()))
+if (!("ggplot2" %in% installed.packages())) {install.packages("ggplot2")};library("ggplot2")
+theme_set(theme_bw() + theme(panel.border = element_blank()))
 
 #' (colors)
 UTblue <- rgb(5/255, 110/255, 167/255, 1) # University of Trier blue corporate color
